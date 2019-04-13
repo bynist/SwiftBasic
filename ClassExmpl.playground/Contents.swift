343c534fbbf5526fdbbe8a3ea8e4503a18400a81
bynist <bayanalbunayan@gmail.com>
2019-04-14 00:13:25 +0300

    last updated

--- ClassExmpl.playground/Contents.swift
+++ ClassExmpl.playground/Contents.swift
@@ -3,35 +3,44 @@
 import Foundation
 
 class Shapes {
-    var height:Double?
-    var weight:Double?
+    var height : Double?
+    var weight : Double?
     
-    func GetArea() -> Double {
-        let area:Double
-        if(weight == nil ){
-            area = height!*height!
-        }else {
-            area = height!*weight!
-        }
-        return area
+    func calArea() -> Double {
+        if let hieght = self.height , let weight = self.weight {  // make sure it have value to calculate
+            return hieght*weight
+        } else{
+            return 0.0  }
     }
     
-}
-class Squar :Shapes {
-    func GetPerimeter()  {
-        let Perimeter = 4*height!
-        print (" Perimeter is:", Perimeter)
+} // end of shape class
+
+class Squar :Shapes {  // inhertence from shape class
+    override func calArea() -> Double {
+        if let hight1 = self.height {
+            return hight1*hight1 }
+        else if let weight1 = self.weight {
+            return weight1*weight1 }
+        else {
+            return 0.0
+        }
+    }
+    
+    func calPerimeter()  {
+        if let Perimeter = self.height {
+            print (" Perimeter is:",  4*Perimeter ) }
     }
 }
 
 
 let squar1 = Squar()
 squar1.height=9
-squar1.GetArea()
-squar1.GetPerimeter()
+squar1.calArea()
+squar1.calPerimeter()
 
 
 let rec = Shapes()
 rec.height=4
 rec.weight=6
-rec.GetArea()
+rec.calArea()
+
