a7b2c3e0139be6ebebbca5c4a4b683954ce651cf
bynist <bayanalbunayan@gmail.com>
2019-04-14 00:16:56 +0300

    updated

--- Struct@basic.playground/Contents.swift
+++ Struct@basic.playground/Contents.swift
@@ -1,23 +1,30 @@
 //: Playground - noun: a place where people can play
 
-
-
 import Foundation
 
 struct Post{
-    var comment:String?
-    var img:URL?
-    var caption:String?
-    var location:URL?
+    var comment : [Comment]?
+    var img : URL?
+    var text : String?
+    var location : String?
+    var owner : String?
+    var createDAT : Data?
     
-    func check() {
-        if let TheComment=comment {
-            print(TheComment)
-        }// need to think&search more for complate
+    
+    func allcomments() -> [Comment] {
+        if let comment = self.comment {
+            return comment
+        }
+        return []
     }
-    
+}
+struct Comment{
+    var text : String?
+    var location : String?
+    var owner : String?
+    var createDAT : Date?
 }
 
 var post1=Post()
-post1.comment = "Hi programmer, I'm join in your club  "
-post1.check()
+//post1.comment = "Hi programmer, I'm join in your club  "
+post1.allcomments
